f983814d36b678ae164a95fc6776f91690569360
bruce.liu <bruce.liu@iceasy.com>
2019-11-14 14:01:10 +0800

    导入model

---
+++ flowable-core/src/main/java/com/dragon/flow/service/flowable/IFlowableIdentityService.java
@@ -0,0 +1,10 @@
+package com.dragon.flow.service.flowable;
+
+/**
+ * @author : bruce.liu
+ * @projectName : flowable
+ * @description: 用户组
+ * @date : 2019/11/1411:46
+ */
+public interface IFlowableIdentityService {
+}
---
+++ flowable-core/src/main/java/com/dragon/flow/service/flowable/impl/FlowableIdentityServiceImpl.java
@@ -0,0 +1,15 @@
+package com.dragon.flow.service.flowable.impl;
+
+import com.dragon.flow.service.flowable.IFlowableIdentityService;
+
+/**
+ * @author : bruce.liu
+ * @title: : FlowableIdentityServiceImpl
+ * @projectName : flowable
+ * @description: 用户组service
+ * @date : 2019/11/1411:46
+ */
+public class FlowableIdentityServiceImpl implements IFlowableIdentityService {
+
+
+}
